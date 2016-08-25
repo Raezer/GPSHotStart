79100859b34b5a0d4c5dcdd7efad940a67bbb39e
Raezer <starosele@gmail.com>
2016-08-25 17:03:44 +0300

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include "stm32f4xx.h"
 #include "mbed.h"
-#include "ubloxOp.h"
-#include "main.h"
-
 
 Serial pc(PA_9,PA_10);
 
+
+int getinfo() {
+	
+    pc.printf("Hello World!\n");
+    while(1) {
+        pc.putc(pc.getc() + 1);
+    }
+}
+
+void init(){
+	pc.baud(9600);
+	device.format(8,no,1);
+  device.reply(0x00); 
+}
+
 int main(void){
 	SystemInit();
-	
+	getinfo();
 	while(1) 
 	{
 		 
@@ -19,10 +31,3 @@ int main(void){
 // tx, rx
 //Serial pc(PA_9, PA_10); 
 
-int getinfo() {
-	
-    pc.printf("Hello World!\n");
-    while(1) {
-        pc.putc(pc.getc() + 1);
-    }
-}
